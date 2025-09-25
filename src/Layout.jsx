d658fb140da5c1da7ed0937328f312a4151d9689
guytito <35842294+GuyTito@users.noreply.github.com>
2025-09-25 01:26:27 +0000

    add registration page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,31 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Navbar from "./components/Navbar" 
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Events from "./pages/Events"
-import Activities from "./pages/Activities"
-import Membership from "./pages/Membership"
-import Contact from "./pages/ContactUs"
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Activities from "./pages/Activities";
+import Membership from "./pages/membership/Membership";
+import Contact from "./pages/ContactUs";
+import Register from "./pages/membership/Register";
 
 const Layout = () => {
-    return(
-        <BrowserRouter>
-           <Navbar /> 
-           <Routes>
-             <Route path="/" element={ <Home />} />
-             <Route path="/about" element={ <About />} /> 
-             <Route path="/events" element={ <Events />} />
-             <Route path="/activities" element={ <Activities />} />
-             <Route path="/membership" element={ <Membership />} />
-             <Route path="/contact" element={ <Contact />} />
-           </Routes>
-         
-            <Footer />
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/membership" element={<Membership />} />
+        <Route path="/membership/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default Layout
+export default Layout;
